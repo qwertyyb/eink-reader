bd5bb5fd0fa72097f9a0a665fce502f42e9df868
qwertyyb <qwertyyb@qq.com>
2023-06-17 18:07:06 +0800

    feat: 添加新的书籍

--- books/index.js
+++ books/index.js
@@ -10,5 +10,35 @@ export const remoteBooks = [
     cover: 'https://via.placeholder.com/300x400?text=cover',
     title: '全球高武',
     downloadUrl: new URL('./全球高武.txt', import.meta.url).href
+  },
+  {
+    id: 3,
+    cover: 'https://via.placeholder.com/300x400?text=cover',
+    title: '遮天',
+    downloadUrl: new URL('./遮天.txt', import.meta.url).href
+  },
+  {
+    id: 4,
+    cover: 'https://via.placeholder.com/300x400?text=cover',
+    title: '完美世界',
+    downloadUrl: new URL('./完美世界.txt', import.meta.url).href
+  },
+  {
+    id: 5,
+    cover: 'https://via.placeholder.com/300x400?text=cover',
+    title: '大奉打更人',
+    downloadUrl: new URL('./大奉打更人.txt', import.meta.url).href
+  },
+  {
+    id: 6,
+    cover: 'https://via.placeholder.com/300x400?text=cover',
+    title: '诡秘之主',
+    downloadUrl: new URL('./诡秘之主.txt', import.meta.url).href
+  },
+  {
+    id: 7,
+    cover: 'https://via.placeholder.com/300x400?text=cover',
+    title: '神墓',
+    downloadUrl: new URL('./神墓.txt', import.meta.url).href
   }
 ]
