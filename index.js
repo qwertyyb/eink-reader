736c16b978fb082a0aaccd4f7951af37e03800fe
qwertyyb <qwertyyb@qq.com>
2023-06-18 16:36:31 +0800

    fix: 优化

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import { createRouter, createWebHashHistory } from 'https://unpkg.com/vue-router
 import VirtualList from 'https://unpkg.com/vue-virtual-list-v3@1.5.1/dist/index.js'
 import IndexPage from './js/pages/index.js'
 import BookPage from './js/pages/book.js'
+import { env } from './js/utils/env.js'
 
 const routes = [
-  { path: '/', name: 'home', component: IndexPage },
-  { path: '/book/:server/:id', name: 'book', component: BookPage, props: true },
+  { path: '/', name: 'home', component: IndexPage, meta: { transitionName: 'slide-right' } },
+  { path: '/book/:server/:id', name: 'book', component: BookPage, props: true, meta: { transitionName: 'slide-left' } },
 ]
 
 const router = createRouter({
@@ -22,3 +23,7 @@ app.use(router)
 app.use(VirtualList)
 
 app.mount('#app')
+
+if (!env.isBooxLeaf()) {
+  document.body.classList.add('enable-anim')
+}
